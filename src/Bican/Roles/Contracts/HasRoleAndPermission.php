0ec543967af9dd7dce2d7e989544ede998dd3fba
Dmitriy Krasnov <dnk@sarnet.ru>
2016-12-25 09:25:05 +0300

    Update HasRoleAndPermission.php

--- src/Bican/Roles/Contracts/HasRoleAndPermission.php
+++ src/Bican/Roles/Contracts/HasRoleAndPermission.php
@@ -27,7 +27,7 @@ interface HasRoleAndPermission
      * @param bool $all
      * @return bool
      */
-    public function is($role, $all = false);
+    public function pass($role, $all = false);
 
     /**
      * Check if the user has all roles.
@@ -111,7 +111,7 @@ interface HasRoleAndPermission
      * @param bool $all
      * @return bool
      */
-    public function can($permission, $all = false);
+    public function may($permission, $all = false);
 
     /**
      * Check if the user has all permissions.
